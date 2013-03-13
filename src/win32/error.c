8898902f904fdd7b7bb06a92f377c620dba31aeb
Jameson Miller Smith <thatjohnsmithdude@microsoft.com>
2013-03-13 17:54:40 -0400

    Report errors from WinHttp

--- src/win32/error.c
+++ src/win32/error.c
@@ -11,15 +11,29 @@
 char *git_win32_get_error_message(DWORD error_code)
 {
 	LPWSTR lpMsgBuf = NULL;
+	DWORD flags =
+		FORMAT_MESSAGE_ALLOCATE_BUFFER |
+		FORMAT_MESSAGE_FROM_SYSTEM |
+		FORMAT_MESSAGE_IGNORE_INSERTS;
+	LPCVOID source = NULL;
 
 	if (!error_code)
 		return NULL;
 
+	/** 
+	 * Error codes 12000 - 12999 are ERROR_INTERNET_*. The following link
+	 * indicates that 12000 - 12175 are for ERROR_INTERNET, but it appears
+	 * there are also internet erros between 12175 - 12999.
+	 * http://msdn.microsoft.com/en-us/library/windows/desktop/ms681384.aspx
+	 */
+	if ((error_code >= 12000) && (error_code <= 12999)) {
+		flags = flags | FORMAT_MESSAGE_FROM_HMODULE;
+		source = GetModuleHandle("winhttp");
+	}
+
 	if (FormatMessageW(
-				FORMAT_MESSAGE_ALLOCATE_BUFFER |
-				FORMAT_MESSAGE_FROM_SYSTEM |
-				FORMAT_MESSAGE_IGNORE_INSERTS,
-				NULL, error_code, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+				flags,
+				source, error_code, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
 				(LPWSTR)&lpMsgBuf, 0, NULL)) {
 		int utf8_size = WideCharToMultiByte(CP_UTF8, 0, lpMsgBuf, -1, NULL, 0, NULL, NULL);
 
